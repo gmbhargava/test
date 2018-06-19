a03466fadc25e20fbad87a026d13efa9db147fef
chenchu Jagadeesh adapala <chenchu.adapala@usbank.com>
2018-06-19 09:38:17 -0500

    New elements in pagefactory for Account dashboard

--- src/test/java/PageFactory/Acountdashboard.java
+++ src/test/java/PageFactory/Acountdashboard.java
@@ -15,11 +15,24 @@ public class Acountdashboard extends Basedriver
 		// TODO Auto-generated constructor stub
 	}
 
-	By lgoutBtn=By.id("Log Out");
-	
+	By lgoutBtn=By.xpath("//XCUIElementTypeButton[@name='Log Out']");
+	By viewTransactionHIstoryBtn=By.xpath("//XCUIElementTypeButton[@name='View Transactions']");
+	By Acount=By.xpath("//XCUIElementTypeStaticText[contains(@name,'AccountLabel')]");
 	public MobileElement logOutButton()
 	{
 		MobileElement lgOutBttn=(MobileElement) driver.findElement(lgoutBtn);
 		return lgOutBttn;
+	}
+	
+	public MobileElement Account()
+	{
+		MobileElement accnt=(MobileElement) driver.findElement(Acount);
+		return accnt;
+		
+	}
+	public MobileElement viewTransactionListButton()
+	{
+		MobileElement transactnBtn=driver.findElement(viewTransactionHIstoryBtn);
+		return transactnBtn;
 	}
 }
